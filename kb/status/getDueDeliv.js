b5c546cdfbe3a0c3db25198abae116435363a538
cajakil <58376424+cajakil@users.noreply.github.com>
2020-10-29 03:21:32 +0000

    try this option button thing

--- kb/status/getDueDeliv.js
+++ kb/status/getDueDeliv.js
@@ -4,13 +4,21 @@ sheetId: '1GiCKVAqQ1n12yJAwSkvZp3sUJo4qK7sMCXWBSVU4Y0o',
 sheetNumber: 7,
 returnAllResults: false,
 }
+function addOpt (opt, name) {
+  var x = document.createElement("OPTION");
+  x.setAttribute("value", "name");
+  var t = document.createTextNode("opt");
+  x.appendChild(t);
+  document.getElementById("deliv-opts").appendChild(x);
+}
 gsheet = GSheetReader(
   options,
   results => {
     // do something with the results here
     console.log(results)
     results.forEach((result) => {
-      document.getElementById("app").innerHTML += `<p>${result["output"]}</p>`
+      // document.getElementById("app").innerHTML += `<p>${result["output"]}</p>`
+      addOpt(result["output"],result["name"])
     });
   },
   error => {
